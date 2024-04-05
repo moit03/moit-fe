b9321c1752c86ffcbc601f648dfc3e3c9b83bf2d
vjiji <restmi0711@gmail.com>
2024-04-05 22:50:35 +0900

    interceptors 토큰 저장 코드 오타 수정

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -9,7 +9,7 @@ instance.interceptors.request.use((config) => {
   const token = localStorage.getItem('accessToken')
   if (token != null) {
     const authConfig = config
-    authConfig.headers.Authorization = `Bareer ${token}`
+    authConfig.headers.Authorization = `Bearer ${token}`
   }
   return config
 })
